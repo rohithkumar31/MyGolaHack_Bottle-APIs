470f67ac079a16b8be56048b191a95c5e106b13a
Suraj Kumar Jana <surajjana2@gmail.com>
2015-07-25 15:54:10 +0530

    new commit

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 import psycopg2
 import urlparse
+import hashlib
 from bottle import Bottle
 
 app = Bottle(__name__)
@@ -26,3 +27,33 @@ def login():
 	records = cur.fetchall()
 
 	return str(records)
+
+@app.route('/signup/<username>/<email>/<phone_no>/<passwd>')
+def login(username,email,phone_no,passwd):
+	conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+	)
+
+	cur = conn.cursor()
+
+	var1 = username
+	var2 = email
+	var3 = phone_no
+	var4 = passwd
+
+	hash_object = hashlib.sha256(str(var4))
+	new_passwd = str(hash_object.hexdigest())
+
+	sql = "INSERT INTO public.\"User\" VALUES ('"+str(var1)+"','"+str(var2)+"','"+str(var3)+"','"+new_passwd+"')"
+
+	cur.execute(sql)
+
+	conn.commit()
+	cur.close()
+	conn.close()
+
+	return "Data Inserted!! :-D"
