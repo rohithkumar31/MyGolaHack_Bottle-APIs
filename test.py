ff1ce6dbb03d552ed3711d662ed5703ef9cbd909
Suraj Kumar Jana <surajjana2@gmail.com>
2015-07-25 20:35:04 +0530

    new commit

--- test.py
+++ test.py
@@ -164,7 +164,7 @@ def up_vote(p_user,p_name):
 		up_vote_count = res[0][0]
 		down_vote_count = res[0][1]
 
-		sql = "UPDATE public.\"Polls\" SET p_up_votes="+str(vote_count+1)+",p_down_votes="+str(down_vote_count-1)+" WHERE p_name='"+str(var2)+"' AND p_user='"+str(var1)+"'"
+		sql = "UPDATE public.\"Polls\" SET p_up_votes="+str(up_vote_count+1)+",p_down_votes="+str(down_vote_count-1)+" WHERE p_name='"+str(var2)+"' AND p_user='"+str(var1)+"'"
 
 		cur.execute(sql)
 
