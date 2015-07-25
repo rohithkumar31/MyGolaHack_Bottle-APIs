37dd3c0c45d24ec379033fb1e01e86ac53fba559
Suraj Kumar Jana <surajjana2@gmail.com>
2015-07-25 15:10:34 +0530

    New commit

--- test.py
+++ test.py
@@ -1,8 +1,28 @@
 import os
+import psycopg2
+import urlparse
 from bottle import Bottle
 
 app = Bottle(__name__)
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-@app.route('/')
-def hello():
-	return "Hello Polling Cube!!"
+@app.route('/test')
+def login():
+	conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+	)
+
+	cur = conn.cursor()
+
+	sql = "SELECT * FROM public.\"User\""
+
+	cur.execute(sql)
+
+	records = cur.fetchall()
+
+	return str(records)
